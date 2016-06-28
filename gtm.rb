1e6b445177c7022434c09dee331feba4922d1551
Michael Schenk <mschenk@edgeg.io>
2016-06-28 17:27:02 -0500

    Update to v1.0-beta.3 and libssh2 dependency

--- gtm.rb
+++ gtm.rb
@@ -1,11 +1,9 @@
 class Gtm < Formula
   desc "Simple, seamless, lightweight time tracking for all your git projects"
   homepage "https://github.com/git-time-metric/gtm"
-  url "https://github.com/git-time-metric/gtm/releases/download/v1.0-beta.2/gtm.v1.0-beta.2.osx.tar.gz"
-  version "beta.2.osx"
-  sha256 "3da4e8fb192931409d7c60ba903deb10da3332090281fe117eecb8c994370343"
-
-  depends_on "libssh2"
+  url "https://github.com/git-time-metric/gtm/releases/download/v1.0-beta.3/gtm.v1.0-beta.3.osx.tar.gz"
+  version "v1.0-beta.3"
+  sha256 "3cd3ee507b1df36d5b959b58ca906c8a4ceff901583b998cc2e597d9a4c99b82"
 
   def install
     bin.install "gtm"
