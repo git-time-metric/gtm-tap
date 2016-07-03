4bd8ad304ae6ff7c58158000c25db9700d8b0db0
Michael Schenk <mschenk@edgeg.io>
2016-07-03 09:02:27 -0500

    Fix checksum

--- gtm.rb
+++ gtm.rb
@@ -3,7 +3,7 @@ class Gtm < Formula
   homepage "https://github.com/git-time-metric/gtm"
   url "https://github.com/git-time-metric/gtm/releases/download/v1.0-beta.4/gtm.v1.0-beta.4.osx.tar.gz"
   version "v1.0-beta.4"
-  sha256 "976a04ff81b9f701d59761d2956a562528c5d809060ad5e3eab6b6e80520f350"
+  sha256 "69b337019126a72ababf478d556d18106cd289dbfe30cee700b824a07f7ed997"
 
   def install
     bin.install "gtm"
