4793a9311f9cfb699e523086e205f4ddeef08f89
Michael Schenk <mschenk@edgeg.io>
2016-08-08 19:05:02 -0500

    Update sha256

--- gtm.rb
+++ gtm.rb
@@ -3,7 +3,7 @@ class Gtm < Formula
   homepage "https://github.com/git-time-metric/gtm"
   url "https://github.com/git-time-metric/gtm/releases/download/v1.0.0/gtm.v1.0.0.osx.tar.gz"
   version "v1.0.0"
-  sha256 ""
+  sha256 "af48370c29fd00cc808e6b46cb2ac96be3c1d34c8bfb8c5e2e164349bc169b6c"
 
   def install
     bin.install "gtm"
