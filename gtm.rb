5bf05f46ec1f13fc5973790c4f3c6e3188f08af2
Michael Schenk <mschenk@edgeg.io>
2016-10-08 17:38:49 -0500

    v1.1.0 Terminal Tracking

--- gtm.rb
+++ gtm.rb
@@ -1,9 +1,9 @@
 class Gtm < Formula
   desc "Simple, seamless, lightweight time tracking for all your git projects"
   homepage "https://github.com/git-time-metric/gtm"
-  url "https://github.com/git-time-metric/gtm/releases/download/v1.1-beta.1/gtm.v1.1-beta.1.osx.tar.gz"
-  version "v1.1-beta.1"
-  sha256 "f9da77d7a9eb41526b937c16afe2ac94c2251bcfac1dc8cfcbf5f597efe88168"
+  url "https://github.com/git-time-metric/gtm/releases/download/v1.1.0/gtm.v1.1.0.osx.tar.gz"
+  version "v1.1.0"
+  sha256 ""
 
   def install
     bin.install "gtm"
