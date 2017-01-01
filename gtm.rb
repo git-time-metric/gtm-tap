ab63ea7512ffc7cebc6a8c1f0904b56937a95f80
Michael Schenk <mschenk@edgeg.io>
2017-01-01 08:49:29 -0600

    Fix sha256

--- gtm.rb
+++ gtm.rb
@@ -3,7 +3,7 @@ class Gtm < Formula
   homepage "https://github.com/git-time-metric/gtm"
   url "https://github.com/git-time-metric/gtm/releases/download/v1.2.4/gtm.v1.2.4.osx.tar.gz"
   version "v1.2.4"
-  sha256 "1adeeef9d25a855955949d91042a3b81efa77da033b302c3f48f0ea058d3ebfd"
+  sha256 "a1d4893153bf4286429ab3186b5fe00fcf478bad9a30677ba9588a687fc6a0c1"
 
   def install
     bin.install "gtm"
