3559c2b9e10f4e03cd8ee538595493573662d28f
Michael Schenk <mschenk@edgeg.io>
2017-03-12 16:19:55 -0500

    gtm 1.2.7

--- gtm.rb
+++ gtm.rb
@@ -1,9 +1,9 @@
 class Gtm < Formula
   desc "Simple, seamless, lightweight time tracking for all your git projects"
   homepage "https://github.com/git-time-metric/gtm"
-  url "https://github.com/git-time-metric/gtm/releases/download/v1.2.6/gtm.v1.2.6.osx.tar.gz"
-  version "v1.2.6"
-  sha256 "9603452e59ab2cfc0e3c49bef1559f315a158e58036b6ecd24256fb7a1b4038a"
+  url "https://github.com/git-time-metric/gtm/releases/download/1.2.7/gtm.1.2.7.osx.tar.gz"
+  version "1.2.7"
+  sha256 "f65df3cc9e92c7db5eed0eccd2920ec5313330109a70f8a6c0b2cea9f55c1497"
 
   def install
     bin.install "gtm"
