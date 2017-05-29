0e753b75f9a37951f1dd132d50a1efbbf559907b
Michael Schenk <mschenk@edgeg.io>
2017-05-28 20:37:45 -0500

    Fix version

--- gtm.rb
+++ gtm.rb
@@ -2,7 +2,7 @@ class Gtm < Formula
   desc "Simple, seamless, lightweight time tracking for all your git projects"
   homepage "https://github.com/git-time-metric/gtm"
   url "https://github.com/git-time-metric/gtm/releases/download/v1.2.9/gtm.v1.2.9.osx.tar.gz"
-  version "1.2.9"
+  version "v1.2.9"
   sha256 "814684ba4537485c34a1b91fdf3afca2ca0e5e4bd055dba8b3f0c176bdaf3dcb"
 
   def install
