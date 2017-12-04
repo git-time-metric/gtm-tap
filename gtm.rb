254e8802fbf4e056b8f2f440e3bbd7c06b7fb7ba
Michael Schenk <michael.schenk@weather.com>
2017-12-03 19:18:39 -0600

    v1.2.11

--- gtm.rb
+++ gtm.rb
@@ -1,9 +1,9 @@
 class Gtm < Formula
   desc "Simple, seamless, lightweight time tracking for all your git projects"
   homepage "https://github.com/git-time-metric/gtm"
-  url "https://github.com/git-time-metric/gtm/releases/download/v1.2.10/gtm.v1.2.10.osx.tar.gz"
-  version "v1.2.10"
-  sha256 "aa97cb916a23244ed3c82b1d44c50cff92dd140a4f3e8c2da14b0936099e2c5e"
+  url "https://github.com/git-time-metric/gtm/releases/download/v1.2.11/gtm.v1.2.11.osx.tar.gz"
+  version "v1.2.11"
+  sha256 "19055831f7823258429e72e9a1965a9662da2bb9a766f3362723c28fdd4dbbdf"
 
   def install
     bin.install "gtm"
