a925ce361e0b4a387df75171f7d159cea1401c96
Michael Schenk <michael.schenk@weather.com>
2018-06-08 18:46:24 -0500

    fix sha

--- gtmlinux.rb
+++ gtmlinux.rb
@@ -3,7 +3,7 @@ class Gtmlinux < Formula
   homepage "https://github.com/git-time-metric/gtm"
   url "https://github.com/git-time-metric/gtm/releases/download/v1.3.3/gtm.v1.3.3.linux.tar.gz"
   version "v1.3.3"
-  sha256 "5a1a924ade70cd412a0dd84b7607cfad3a07754f8fa14dea2440f182cc5884ba"
+  sha256 "0022ccde788ac85f4648338ddfd4bd20cdd8dd9adc0232cc52d453d2f27cdd6f"
 
   def install
     bin.install "gtm"
