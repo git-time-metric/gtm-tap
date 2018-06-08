e7081bcb7c45d84197e831f83bbfd1336fa80080
Michael Schenk <michael.schenk@weather.com>
2018-06-08 18:28:22 -0500

    brew linux formula

---
+++ gtmlinux.rb
@@ -0,0 +1,12 @@
+class GtmLinux < Formula
+  desc "Simple, seamless, lightweight time tracking for all your git projects"
+  homepage "https://github.com/git-time-metric/gtm"
+  url "https://github.com/git-time-metric/gtm/releases/download/v1.3.3/gtm.v1.3.3.linux.tar.gz"
+  version "v1.3.3"
+  sha256 "5a1a924ade70cd412a0dd84b7607cfad3a07754f8fa14dea2440f182cc5884ba"
+
+  def install
+    bin.install "gtm"
+  end
+
+end
