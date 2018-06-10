355a18d475b8298676f0d8df00b564226f799ab7
Michael Schenk <michael.schenk@weather.com>
2018-06-10 14:45:09 -0500

    remove str interpolation

--- gtm.rb
+++ gtm.rb
@@ -4,10 +4,10 @@ class Gtm < Formula
   version 'v1.3.3'
 
   if OS.mac ?
-      url "https://github.com/git-time-metric/gtm/releases/download/#{version}/gtm.#{version}.osx.tar.gz"
+      url 'https://github.com/git-time-metric/gtm/releases/download/v1.3.3/gtm.v1.3.3.osx.tar.gz'
       sha256 '5a1a924ade70cd412a0dd84b7607cfad3a07754f8fa14dea2440f182cc5884ba'
   elsif OS.linx ?
-      url "https://github.com/git-time-metric/gtm/releases/download/#{version}/gtm.#{version}.linux.tar.gz"
+      url 'https://github.com/git-time-metric/gtm/releases/download/v1.3.3/gtm.v1.3.3.linux.tar.gz'
       sha256 '0022ccde788ac85f4648338ddfd4bd20cdd8dd9adc0232cc52d453d2f27cdd6f'
   end
 
