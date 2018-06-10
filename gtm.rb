6595cdf39727fb54937b8bbe49e4af00f919ddf2
Michael Schenk <michael.schenk@weather.com>
2018-06-10 16:26:05 -0500

    fix check for linux

--- gtm.rb
+++ gtm.rb
@@ -7,7 +7,7 @@ class Gtm < Formula
   if OS.mac? then
       os = 'osx'
       sha256 '5a1a924ade70cd412a0dd84b7607cfad3a07754f8fa14dea2440f182cc5884ba'
-  elsif OS.linx? then
+  elsif OS.linux? then
       os = 'linux'
       sha256 '0022ccde788ac85f4648338ddfd4bd20cdd8dd9adc0232cc52d453d2f27cdd6f'
   end
