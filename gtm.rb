96b7fa54a87de3f61b7c23c19a6cf64e8c4e62f5
Michael Schenk <michael.schenk@weather.com>
2018-06-10 15:04:41 -0500

    make it os a variable

--- gtm.rb
+++ gtm.rb
@@ -3,16 +3,17 @@ class Gtm < Formula
   homepage 'https://github.com/git-time-metric/gtm'
   version 'v1.3.3'
 
+  os = ''
   if OS.mac? then
-      os 'osx'
+      os = 'osx'
       # url "https://github.com/git-time-metric/gtm/releases/download/#{Gtm.version}/gtm.#{Gtm.version}.osx.tar.gz"
       sha256 '5a1a924ade70cd412a0dd84b7607cfad3a07754f8fa14dea2440f182cc5884ba'
   elsif OS.linx? then
-      os 'linux'
+      os = 'linux'
       # url "https://github.com/git-time-metric/gtm/releases/download/#{Gtm.version}/gtm.#{Gtm.version}.linux.tar.gz"
       sha256 '0022ccde788ac85f4648338ddfd4bd20cdd8dd9adc0232cc52d453d2f27cdd6f'
   end
-  url "https://github.com/git-time-metric/gtm/releases/download/#{Gtm.version}/gtm.#{Gtm.version}.#{Gtm.os}.tar.gz"
+  url "https://github.com/git-time-metric/gtm/releases/download/#{Gtm.version}/gtm.#{Gtm.version}.#{os}.tar.gz"
 
   def install
     bin.install 'gtm'
