88ee220747ea154bff9ed0c7c3360c55553316a6
Michael Schenk <michael.schenk@weather.com>
2018-06-10 15:07:00 -0500

    one formula for both linux and osx

--- gtm.rb
+++ gtm.rb
@@ -6,11 +6,9 @@ class Gtm < Formula
   os = ''
   if OS.mac? then
       os = 'osx'
-      # url "https://github.com/git-time-metric/gtm/releases/download/#{Gtm.version}/gtm.#{Gtm.version}.osx.tar.gz"
       sha256 '5a1a924ade70cd412a0dd84b7607cfad3a07754f8fa14dea2440f182cc5884ba'
   elsif OS.linx? then
       os = 'linux'
-      # url "https://github.com/git-time-metric/gtm/releases/download/#{Gtm.version}/gtm.#{Gtm.version}.linux.tar.gz"
       sha256 '0022ccde788ac85f4648338ddfd4bd20cdd8dd9adc0232cc52d453d2f27cdd6f'
   end
   url "https://github.com/git-time-metric/gtm/releases/download/#{Gtm.version}/gtm.#{Gtm.version}.#{os}.tar.gz"
