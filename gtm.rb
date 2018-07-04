1801a35c84e26fca458c5e9809ac22ccd13bc36c
Michael Schenk <michael.schenk@weather.com>
2018-07-04 13:38:42 -0500

    v1.3.4

--- gtm.rb
+++ gtm.rb
@@ -1,15 +1,18 @@
 class Gtm < Formula
   desc 'Simple, seamless, lightweight time tracking for all your git projects'
   homepage 'https://github.com/git-time-metric/gtm'
-  version 'v1.3.3'
+
+  version 'v1.3.4'
+  sha256Mac = 'd6337d36d846993c061bc2a5f82b6cb4b35ddce129430a7119140016bdb69dd8'
+  sha256Linux = '0e4e6952f4482ec9e0c55331396b550463da0ec46134c95f3d905a52da7382f1'
 
   os = ''
   if OS.mac? then
       os = 'osx'
-      sha256 '5a1a924ade70cd412a0dd84b7607cfad3a07754f8fa14dea2440f182cc5884ba'
+      sha256 "#{sha256Mac}"
   elsif OS.linux? then
       os = 'linux'
-      sha256 '0022ccde788ac85f4648338ddfd4bd20cdd8dd9adc0232cc52d453d2f27cdd6f'
+      sha256 "#{sha256Linux}"
   end
   url "https://github.com/git-time-metric/gtm/releases/download/#{Gtm.version}/gtm.#{Gtm.version}.#{os}.tar.gz"
 
